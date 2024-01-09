a9941bbc4f1bffe2cbf8d3129437f7abd2b81f27
ggiudice98 <giancarlogiudice@live.com>
2024-01-09 01:48:40 -0500

    last edit

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "44fea9efb7b6bc4fb28693db70294006"
+weather_api_key ="" 
 
 # Geoapify API Key
-geoapify_key = "b338c08c947a4950b48e3551af901047"
+geoapify_key = ""
